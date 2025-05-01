92b139797544311c7fc3dc208ed157e5c343042b
Rempaah <rmuhammadreff@gmail.com>
2025-05-01 11:49:51 +0700

    blabla

--- App/main.cpp
+++ App/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_VULKAN
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
-	std::cin.get();
+	GLFWwindow* window = nullptr;
+	if (!glfwInit)
+		return -1;
+
+	window = glfwCreateWindow(1280, 720, "App", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
